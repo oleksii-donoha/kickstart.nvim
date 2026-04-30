57fa2f447872cf41ba0545f97fe51f48f6bef819
Oleksii Donoha <55483143+oleksii-donoha@users.noreply.github.com>
2026-04-30 11:57:06 +0200

    chore: upd 04.2026.1

--- init.lua
+++ init.lua
@@ -221,6 +221,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>vv', '<cmd>vsplit<CR>', { desc = '[V]ertical split' })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git<CR>', { desc = 'Lazy[G]it' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
